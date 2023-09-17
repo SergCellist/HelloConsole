582fb929f01d711b93200606a1afd2ddd498ba22
SergeyCellist1991 <serg311091@gmail.com>
2023-09-18 01:59:24 +0300

    Добавили операторы if/else и операцию .ToLower()

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter your name");
+string UserName = Console.ReadLine();
+if (UserName.ToLower() == "sergey") // Операция .ToLower() - позволяет перевести все символы нашей строки в нижний регистр.
+{
+    Console.WriteLine("Ура , это же Сергей");
+}
+else
+{
+    Console.WriteLine("Привет, " + UserName);
+}
